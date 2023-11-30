0ed880cec950b8cb93a4235f4706f6dc777bcee9
HarryF1204 <harryf1204@protonmail.com>
2023-11-30 19:34:17 +0000

    test

--- jsonc/__init__.py
+++ jsonc/__init__.py
@@ -1,8 +1,18 @@
 import encoder
 import decoder
 
-__all__ = ["dump"]
+__all__ = ["dump", "dumps", "load", "loads"]
 
 
 def dump():
+    pass
+
+def dumps():
+    pass
+
+
+def load():
+    pass
+
+def loads():
     pass
