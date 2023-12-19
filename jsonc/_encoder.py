6751ad86722e1f7ca13502bace172d868869cce3
Kirby <dev.yesitskirby@gmail.com>
2023-12-19 01:10:39 +0000

    comments can now contain json

--- jsonc/_encoder.py
+++ jsonc/_encoder.py
@@ -1,5 +1,6 @@
 import re
-from ._textUtils import getNonWhitespaceChar
+from ._textUtils import getNonWhitespaceChar, getPrevBracket
+from collections import deque
 
 
 class _JsonCEncoder:
@@ -14,9 +15,32 @@ class _JsonCEncoder:
         self._doubleCommaPatten = re.compile(
             r'(,\s*,)',
             re.DOTALL | re.MULTILINE)
+        self._removeComments = re.compile(
+            r'(//[^\n]*)|(/\*.*?\*/)',
+            re.DOTALL | re.MULTILINE
+        )
         self._jsonCData = ""
 
     def JsonCToJson(self, jsonCData) -> str:
+        def isInsideArray(index) -> str | None:
+            stack = deque()
+
+            rev_data = self._jsonCData[:index]
+            rev_data = self._removeComments.sub('', rev_data)[::-1]  # Incase there is any JSON in the comments
+
+            isInString = False
+            for char in rev_data:
+                if char == '"':
+                    isInString = not isInString
+                if char in (']', '}') and not isInString:
+                    stack.append(char)
+                elif char in ('[', '{') and not isInString:
+                    if not stack:
+                        return char
+
+                    stack.pop()
+            return None
+
         def encodeComments(match) -> str:
             nextChar = getNonWhitespaceChar(self._jsonCData, match.end())
             prevChar = getNonWhitespaceChar(self._jsonCData, match.start(), reverse=True)
@@ -26,8 +50,8 @@ class _JsonCEncoder:
             isCommaBefore = True if prevChar != ',' else False
             isCommaAfter = True if nextChar not in {',', ']', '}'} else False
 
-            arrayDepth = self._jsonCData.count('[', 0, match.start()) - self._jsonCData.count(']', 0, match.start())
-            isInArray = arrayDepth > 0
+            isInArray = isInsideArray(match.start())
+            isInArray = True if isInArray == '[' else False
 
             commentId = f'{match.start()}_{match.end()}'
             commentContent = match.group().replace('\n', '\\n').replace('\t', '\\t').replace('"', "'")
@@ -39,7 +63,7 @@ class _JsonCEncoder:
                 f'"{commentContent}",',
                 '"__is_inline": ',
                 str(str(isInline).lower()),
-                '}' if isInArray else '}}',
+                '}' if isInArray else '}',
                 f'{"," if isCommaAfter else ""}'
             ])
 
@@ -48,4 +72,5 @@ class _JsonCEncoder:
         self._jsonCData = jsonCData.strip("''")
         jsonData = self._encodeCommentPattern.sub(encodeComments, self._jsonCData)
         jsonData = self._doubleCommaPatten.sub(',', jsonData)
+
         return jsonData
